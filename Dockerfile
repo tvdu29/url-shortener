a8c37b6f49073b9080df26d1ca7a0f6acf4b1d77
tmaze <tmaze@student.42.fr>
2019-08-15 14:48:13 +0200

    Pass OK
    added gunicorn WSGI server in docker image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM python:slim-stretch
+FROM debian
 
-RUN pip install flask
-COPY ./url-shortener.py /
-CMD "flask run -p 8080 /url-shortener.py"
+ENV PORT 5000
+ENV NB_WORKERS 1
+
+WORKDIR /usr/src/app
+
+RUN apt update;\
+	apt install -y python3 python3-pip;\
+	python3 -m pip install flask gunicorn;
+COPY ./url-shortener.py ./
+CMD gunicorn -w ${NB_WORKERS} -b 0.0.0.0:${PORT} url-shortener:app
--- url-shortener.py
+++ url-shortener.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, Response
 app = Flask(__name__)
 
 urls = {}
@@ -8,15 +8,15 @@ def generate():
     global urls
     data = request.get_data(cache=False, as_text=True)
     i = 1
-    while (i in urls):
+    while (str(i) in urls):
         i += 1
-    urls[i] = data
+    urls[str(i)] = data
     return ("%d" % i)
 
-@app.route('/follow/<int:urlid>')
+@app.route('/follow/<string:urlid>')
 def follow(urlid):
     if (urlid in urls):
         return redirect(urls[urlid])
+    else:
+        return Response("<h1>URL "+urlid+" not found</h1>", status=404, mimetype="text/html")
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', 8080)
