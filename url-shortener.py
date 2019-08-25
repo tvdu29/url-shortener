93f46df5995a47eaf029be9a68ddd0cb3caf695f
tmaze <tmaze@student.42.fr>
2019-08-25 23:46:56 +0200

    added csv file management

--- url-shortener.py
+++ url-shortener.py
@@ -1,20 +1,34 @@
 from flask import Flask, redirect, request, Response
+import csv
 app = Flask(__name__)
-
-urls = {}
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    global urls
+    urls = {}
     data = request.get_data(cache=False, as_text=True)
-    i = 1
-    while (str(i) in urls):
-        i += 1
-    urls[str(i)] = data
-    return ("%d" % i)
+    with open("/data/urls.csv", "r") as file:
+        reader = csv.reader(file, delimiter=',')
+        for k, v in reader:
+            urls[k] = v
+        file.close()
+        i = 1
+        while (str(i) in urls):
+            i += 1
+        urls[str(i)] = data
+        with open("/data/urls.csv", "a") as file:
+            writer = csv.writer(file)
+            writer.writerow([str(i), data])
+            file.close()
+            return ("%d" % i)
 
 @app.route('/follow/<string:urlid>')
 def follow(urlid):
+    urls = {}
+    with open("/data/urls.csv", "r") as file:
+        reader = csv.reader(file, delimiter=',')
+        for k, v in reader:
+            urls[k] = v
+        file.close()
     if (urlid in urls):
         return redirect(urls[urlid])
     else:
